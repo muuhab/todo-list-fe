3f5b8c6ea73c4bad19e2b2758d97b39a8ede6d3f
Mohab Sherif <mohab.barca@gmail.com>
2024-05-19 21:32:18 +0300

    feat: Added todo crud

---
+++ components/todo-item.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { BASE_URL } from '@/config/datasource';
+import { useTodoList } from '@/hooks/use-todo-list';
+import Image from 'next/image';
+import { FC } from 'react';
+import { TodoItemInterface as TodoType } from './todo';
+
+interface TodoItemProps {
+    item: TodoType;
+    setShowInput?: (value: boolean) => void;
+}
+
+const TodoItem: FC<TodoItemProps> = ({ item, setShowInput }) => {
+
+    const {
+        form,
+        focus,
+        open,
+        handleBlur,
+        focusItem,
+        openItem,
+        removeItem,
+        onFileChange
+    } = useTodoList(item, setShowInput)
+
+    return <form
+        tabIndex={0}
+        onBlur={handleBlur}
+        onClick={(e) => { e.stopPropagation(); focusItem(); }}
+        onDoubleClick={() => openItem()}
+        className={`px-2 py-4 rounded transition-all flex text-md bg-gray-700 ${focus && 'bg-indigo-800'} shadow-lg  my-10 -mx-2 ${!open && 'mb-1 cursor-pointer px-4 '}`}
+    >
+        <div className="flex-none w-10 leading-none">
+            <input type="checkbox"
+                {...form.register('completed')}
+            />
+        </div>
+        <div className="flex-grow max-w-full">
+            <div className="w-full leading-none">
+                <h3 className={`text-md leading-none truncate w-full pr-10 ${item.title?.length ? 'text-gray-300' : 'text-gray-500'}`} style={{ display: open ? 'none' : 'block' }}>
+                    {form.getValues('title') || 'New todo...'}
+                </h3>
+                <input
+                    type="text"
+                    style={{ display: open ? 'block' : 'none' }}
+                    className="text-md w-full bg-transparent text-gray-300 leading-none focus:outline-none mb-2"
+                    placeholder="New todo..."
+                    {...form.register('title')}
+                />
+            </div>
+            <div className="w-full" style={{ display: open ? 'block' : 'none' }}>
+                <textarea
+                    className="text-md w-full bg-transparent text-gray-300 leading-tight focus:outline-none"
+                    {...(form.register('description'))}
+                    placeholder="Notes"
+                />
+            </div>
+            <div style={{ display: open ? 'flex' : 'none' }} className='flex gap-2  py-4'>
+                {
+                    item?.attachments?.map((attachment, index) => {
+                        return <div key={index} className="flex flex-col  items-start cursor-pointer">
+                            <Image src="/icons/pdf-svgrepo-com.svg" onClick={() => {
+                                window.open(`${BASE_URL}/${attachment?.path}`)
+                            }} width={30} height={30} alt="pdf" />
+                            <p className='text-xs'>{attachment?.name}</p>
+                        </div>
+                    }
+                    )
+                }
+            </div>
+            <div className="flex  justify-end items-center gap-8 ">
+
+                <div style={{ display: open ? 'flex' : 'none' }}>
+
+                    <input
+                        type="file" onChange={onFileChange}
+                        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" multiple />
+                </div>
+                {
+                    item.id &&
+
+                    <div className=" flex justify-end" style={{ display: open ? 'flex' : 'none' }}>
+                        <button className="p-1 mr-4 focus:outline-none hover:text-red-300"
+                            onClick={removeItem}
+                        >
+                            <Image src="/icons/trash-bin-2-svgrepo-com.svg" width={20} height={20} alt="Add" />
+                        </button>
+                    </div>
+                }
+            </div>
+        </div>
+    </form>
+
+}
+
+export default TodoItem
---
+++ components/todo.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import { GetTodos } from '@/actions/todoAPis';
+import Image from 'next/image';
+import { createRef, useEffect, useState } from 'react';
+import TodoItem from './todo-item';
+import * as z from 'zod';
+
+
+export interface TodoItemInterface {
+    id: number | undefined;
+    title: string;
+    description?: string;
+    completed: boolean | number;
+    attachments?: Record<string, any>[];
+    focused?: boolean;
+    open?: boolean;
+}
+
+export const formSchema = z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    completed: z.boolean(),
+
+});
+
+export type formSchemaType = z.infer<typeof formSchema>
+
+
+
+const Todo = () => {
+
+    const { data: todos, isFetched, isLoading } = GetTodos()
+
+    const [showInput, setShowInput] = useState(false)
+    const lastRef = createRef<HTMLLIElement>();
+
+    useEffect(() => {
+        if (lastRef.current)
+            lastRef?.current.scrollIntoView({ behavior: "smooth" });
+    }, [lastRef]);
+
+
+
+
+    const addItem = () => {
+        setShowInput(true)
+
+    };
+
+
+    return (
+        <div className="overflow-x-hidden  min-h-screen  bg-gray-800 flex items-center justify-center px-5 py-5">
+            <div className="w-full mx-auto  rounded-lg border border-gray-700 p-8 lg:py-12 lg:px-14 text-gray-300 " style={{ maxWidth: '800px' }}>
+                <div className="mb-10">
+                    <h1 className="text-2xl font-bold">
+                        Todos
+                    </h1>
+                </div>
+                <div className="mb-10 ">
+                    {isLoading ? <div className='h-full w-full flex justify-center items-center'>
+                        <Image src="/icons/loading-svgrepo-com.svg" className='animate-spin' width={50} height={50} alt="Add" />
+                    </div> :
+                        isFetched && todos.data?.length ? (
+                            <ul >
+                                {todos?.data.map((item: TodoItemInterface, index: number) => {
+                                    if (index === todos?.data?.length - 1) {
+                                        return <li
+                                            key={index}
+                                            ref={lastRef}
+                                        >
+                                            <TodoItem
+                                                item={item}
+                                            />
+
+                                        </li>
+                                    }
+                                    else {
+                                        return <li
+                                            key={index}
+                                        >
+                                            <TodoItem
+                                                item={item}
+                                            />
+
+                                        </li>
+                                    }
+                                }
+                                )}
+                            </ul>
+                        ) : (
+                            <p className="text-gray-500">No todos</p>
+                        )}
+                    {
+                        showInput && <TodoItem
+                            setShowInput={setShowInput}
+                            item={{ id: undefined, title: '', completed: false, open: true }}
+                        />
+                    }
+                </div>
+                {
+                    !isLoading &&
+                    <div className="flex justify-center">
+                        <button className="py-1 px-10 border border-gray-800 hover:border-gray-700 rounded leading-none focus:outline-none text-xl"
+                            onClick={addItem}
+                        >
+                            <Image src="/icons/plus-svgrepo-com.svg" width={24} height={24} alt="Add" />
+                        </button>
+                    </div>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Todo;
